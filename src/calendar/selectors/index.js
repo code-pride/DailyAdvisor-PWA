423152e75140adef30b9e6a81fdd4707c0bd3b05
Tomasz Ferens <tomaszferens@hotmail.com>
2018-08-15 17:31:28 +0200

    Calculate and merge trainings daily, weekly and monthly

--- src/calendar/selectors/index.js
+++ src/calendar/selectors/index.js
@@ -1,101 +1,173 @@
-import { createSelector } from "reselect";
-import moment from "moment";
-import _range from "lodash/range";
+import { createSelector } from 'reselect';
+import moment from 'moment';
+import _range from 'lodash/range';
+import _defaults from 'lodash/defaults';
+import _isEmpty from 'lodash/isEmpty';
 
-import { data } from "../data";
+import { data } from '../data';
 
 export const yearSelector = createSelector(
-  state => state.calendar.month,
-  month =>
-    moment()
-      .month(month)
-      .year()
+    state => state.calendar.month,
+    month =>
+        moment()
+            .month(month)
+            .year(),
 );
 
 export const startOfMonthSelector = createSelector(
-  state => state.calendar.month,
-  month =>
-    moment()
-      .month(month)
-      .startOf("month")
-      .dayOfYear()
+    state => state.calendar.month,
+    month =>
+        moment()
+            .month(month)
+            .startOf('month')
+            .dayOfYear(),
 );
 
 export const endOfMonthSelector = createSelector(
-  state => state.calendar.month,
-  month =>
-    moment()
-      .month(month)
-      .endOf("month")
-      .dayOfYear()
+    state => state.calendar.month,
+    month =>
+        moment()
+            .month(month)
+            .endOf('month')
+            .dayOfYear(),
 );
 
 export const daysSelector = createSelector(
-  state => state.calendar.month,
-  startOfMonthSelector,
-  endOfMonthSelector,
-  (month, startOfMonth, endOfMonth) => {
-    return _range(startOfMonth, endOfMonth + 1).map(day => ({
-      name: moment()
-        .month(month)
-        .dayOfYear(day)
-        .format("D dddd"),
-      unique: `${month}${day}`,
-      day
-    }));
-  }
+    state => state.calendar.month,
+    startOfMonthSelector,
+    endOfMonthSelector,
+    (month, startOfMonth, endOfMonth) => {
+        return _range(startOfMonth, endOfMonth + 1).map(day => ({
+            name: moment()
+                .month(month)
+                .dayOfYear(day)
+                .format('D dddd'),
+            unique: `${month}${day}`,
+            day,
+        }));
+    },
 );
 
 export const monthNameSelector = createSelector(
-  state => state.calendar.month,
-  month =>
-    moment()
-      .month(month)
-      .format("MMMM")
+    state => state.calendar.month,
+    month =>
+        moment()
+            .month(month)
+            .format('MMMM'),
 );
 
+const dayWeekSelectors = createSelector(
+    state => state.calendar.month,
+    startOfMonthSelector,
+    endOfMonthSelector,
+    (month, startOfMonth, endOfMonth) =>
+        _range(startOfMonth, endOfMonth + 1).map(day => ({
+            ofYear: day,
+            ofWeek: moment()
+                .month(month)
+                .dayOfYear(day)
+                .weekday(),
+        })),
+);
+
+const mergeTrainings = (...allTrainings) =>
+    allTrainings.reduce((trainings, trainingsByRecurringType) => {
+        return {
+            ...trainings,
+            ...Object.entries(trainingsByRecurringType).reduce((trainingsInDay, [key, value]) => {
+                return {
+                    ...trainingsInDay,
+                    [key]: [
+                        ..._defaults(trainingsInDay[key], {}),
+                        ..._defaults(trainings[key], {}),
+                        ...value,
+                    ],
+                };
+            }, {}),
+        };
+    }, {});
+
+const filterByRecurringName = (recurringName, date) => t =>
+    t.event.recurringPattern.recurringType.recurringName === recurringName &&
+    date.isBetween(t.event.startDate, t.event.endDate);
+
+const calculateDailyTrainings = (trainings, month, day) => {
+    return trainings.reduce((acc, t) => {
+        const key = `${month}${day}`;
+        if (!acc[key]) {
+            acc[key] = {};
+        }
+
+        return {
+            ...acc,
+            [key]: [...acc[key], t],
+        };
+    }, {});
+};
+
+const calculateWeeklyTrainings = (trainings, month, day) => {
+    const matchedTrainings = trainings.filter(
+        t => t.event.recurringPattern.day_of_week === day.ofWeek,
+    );
+
+    return !_isEmpty(matchedTrainings)
+        ? {
+              [`${month}${day.ofYear}`]: matchedTrainings,
+          }
+        : {};
+};
+
+const calculateMonthlyTrainings = (trainings, month, day, days) => {
+    const matchedTrainings = trainings.filter(t => {
+        const start = moment(t.event.startDate).dayOfYear();
+        const matchedDay = days
+            .filter(day => day.ofYear >= start)
+            .find(d => t.event.recurringPattern.day_of_week === d.ofWeek);
+
+        return matchedDay.ofYear === day.ofYear;
+    });
+
+    return !_isEmpty(matchedTrainings)
+        ? {
+              [`${month}${day.ofYear}`]: matchedTrainings,
+          }
+        : {};
+};
+
 export const trainingDaysSelector = createSelector(
-  state => state.calendar.month,
-  startOfMonthSelector,
-  endOfMonthSelector,
-  (month, startOfMonth, endOfMonth) => {
-    return _range(startOfMonth, endOfMonth + 1).reduce((acc, day) => {
-      const pastDayWithoutTraining = data.trainings.every(
-        training =>
-          moment(training.event.startDate).unix() >
-          moment()
-            .month(month)
-            .dayOfYear(day)
-            .unix()
-      );
+    state => state.calendar.month,
+    dayWeekSelectors,
+    (month, days) => {
+        const trainingDays = days.reduce((acc, day) => {
+            const date = moment()
+                .month(month)
+                .dayOfYear(day.ofYear);
 
-      if (pastDayWithoutTraining) {
-        return acc;
-      }
+            const isNotInRange = data.trainings.every(
+                t => !date.isBetween(t.event.startDate, t.event.endDate),
+            );
 
-      const dayOfWeek = moment()
-        .month(month)
-        .dayOfYear(day)
-        .weekday();
+            if (isNotInRange) {
+                return acc;
+            }
 
-      const trainings = data.trainings.map(training => ({
-        dayOfWeek: training.event.recurringPattern.day_of_week,
-        trainText: training.trainText,
-        endDate: training.event.endDate
-      }));
+            const daily = data.trainings.filter(filterByRecurringName('daily', date));
+            const dailyTrainings = calculateDailyTrainings(daily, month, day.ofYear);
 
-      const training = trainings.find(e => e.dayOfWeek === dayOfWeek);
+            const weekly = data.trainings.filter(filterByRecurringName('weekly', date));
+            const weeklyTrainings = calculateWeeklyTrainings(weekly, month, day);
 
-      if (
-        training &&
-        moment()
-          .month(month)
-          .dayOfYear(day)
-          .unix() < moment(training.endDate).unix()
-      ) {
-        return [...acc, `${month}${day}`];
-      }
-      return acc;
-    }, []);
-  }
+            const monthly = data.trainings.filter(filterByRecurringName('monthly', date));
+            const monthlyTrainings = calculateMonthlyTrainings(monthly, month, day, days);
+
+            return {
+                ...acc,
+                daily: { ..._defaults(acc.daily, {}), ...dailyTrainings },
+                weekly: { ..._defaults(acc.weekly, {}), ...weeklyTrainings },
+                monthly: { ..._defaults(acc.monthly, {}), ...monthlyTrainings },
+            };
+        }, {});
+
+        return mergeTrainings(...Object.values(trainingDays));
+    },
 );
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,11 +1,11 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import { userReducer } from "./user/reducers/";
-import { authReducer } from "./auth/reducers";
-import { calendar } from "./calendar/reducers/calendar";
+import { userReducer } from './user/reducers/';
+import { authReducer } from './auth/reducers';
+import { calendar } from './Calendar/reducers/calendar';
 
 export const rootReducer = combineReducers({
-  authReducer,
-  userReducer,
-  calendar
+    authReducer,
+    userReducer,
+    calendar,
 });
