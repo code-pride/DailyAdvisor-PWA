916219265a4cd6193083b82bb3b9b59e0d44d616
Marcin Krawczyk <marckraw@icloud.com>
2018-09-26 22:57:23 +0200

    Add example data

---
+++ src/advertisement/mockedData.ts
@@ -0,0 +1,7 @@
+export const exampleAdvertisementData = {
+    bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo iaculis arcu vitae ultricies. Donec convallis, leo ut convallis ornare, lacus odio bibendum nisl, nec porttitor orci nibh sed augue. Donec blandit nibh at felis tincidunt laoreet. Nam id pellentesque nibh, id facilisis lacus. Sed ac nibh est. Sed fermentum, tellus quis malesuada pellentesque, tortor lectus dictum massa, ac rutrum mi nisi lacinia mi. Phasellus erat eros, porttitor pellentesque facilisis ac, dapibus non arcu. Aenean faucibus velit non nisi tempus, ut scelerisque velit dignissim.
+    Maecenas et eros condimentum, commodo augue in, elementum turpis. Nam ac sem facilisis, mattis felis nec, ultrices velit. Phasellus vitae augue vel nulla sollicitudin feugiat ut in est. Nullam ut pretium ex. Mauris sit amet velit porttitor, vestibulum odio ut, pharetra ipsum. Vivamus metus leo, pretium vel erat eget, placerat feugiat libero. Duis viverra at risus id lacinia. Mauris nec leo volutpat, elementum augue in, efficitur eros. Curabitur euismod mauris sit amet pulvinar tincidunt. Pellentesque posuere sed velit vel molestie. Vestibulum blandit lorem varius mi vestibulum, eget laoreet mauris tristique. Aenean sit amet tellus semper, imperdiet sem eget, consectetur urna.`,
+    workDays: ['0, 2, 3, 6'],
+    yearsOfExp: 3,
+    discipline: 'Running',
+};
